7d662d2896a62d7f231df8cc10bd0e3d599a2757
RaidTheWeb <therealraidtheweb@gmail.com>
2021-08-06 13:17:30 +1200

    Add 'RAM' info to data

--- synfetch.c
+++ synfetch.c
@@ -2,6 +2,39 @@
 #include <stdlib.h>
 #include <sys/utsname.h>
 #include <synnixos.h>
+#include <math.h>
+
+char * calculateSize( off_t size ) {
+    char *result = (char *) malloc(sizeof(char) * 20);
+    static int GB = 1024 * 1024 * 1024;
+    static int MB = 1024 * 1024;
+    static int KB = 1024;
+    if (size >= GB) {
+        if (size % GB == 0)
+            sprintf(result, "%d GB", size / GB);
+        else
+            sprintf(result, "%d GB", size / GB);
+    }
+    else if (size >= MB) {
+        if (size % MB == 0)
+            sprintf(result, "%d MB", size / MB);
+        else
+            sprintf(result, "%d MB", size / MB);
+    }
+    else {
+        if (size == 0) {
+            result[0] = '0';
+            result[1] = '\0';
+        }
+        else {
+        if (size % KB == 0)
+            sprintf(result, "%d KB", size / KB);
+        else
+            sprintf(result, "%d KB", size / KB);
+        }
+    }
+    return result;
+}
 
 int main(int argc, const char** argv) {
     printf("\x1b[1;32m____ _   _ _  _ _  _ _ _  _ \x1b[0m____ ____ \n");
@@ -14,8 +47,7 @@ int main(int argc, const char** argv) {
     printf("\x1b[1;34mOS:\x1b[0m %s %s\n", buf.sysname, buf.machine);
     printf("\x1b[1;34mHostname:\x1b[0m %s\n", buf.nodename);
     printf("\x1b[1;34mKernel:\x1b[0m %s\n", buf.release);
-    printf("\x1b[1;34mShell:\x1b[0m %s\n", "SynnixOS Shell");
-
+    
     long milli = xtime();
 
     long days = milli / (1000 * 60 * 60 * 24);
@@ -24,6 +56,7 @@ int main(int argc, const char** argv) {
     long seconds = (milli % (1000 * 60)) / 1000;
 
     printf("\x1b[1;34mUptime:\x1b[0m %d days, %d hours, %d minutes, %d seconds\n", days, hours, minutes, seconds);
-
+    printf("\x1b[1;34mShell:\x1b[0m %s\n", "SynnixOS Shell");
+    printf("\x1b[1;34mRAM:\x1b[0m %s / %s\n", calculateSize(totalmem() - leakedmem() - freemem()), calculateSize(totalmem()));
     return 0;
 }
